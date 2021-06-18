57574d730e0b369f9babf9b11056116e675e3145
FantaWauWau <manuel.reynders@arcor.de>
2021-06-18 20:20:57 +0200

    Update requests approach.py
    Added automatic inventory loader, filter (weapon name, event) to request price for

--- requests approach.py
+++ requests approach.py
@@ -1,16 +1,44 @@
 import requests
-import csv
+import time
 
-#creates a list of all item names in the csvfile
-item_names = []
-with open("IEM_Stickerprice.csv", newline="") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        item_names.append(row["Sticker"])
 
+item_list = []
 data_list = []
-for item_name in item_names:
-    response = requests.get('https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name='+item_name)
-    data = response.json()
-    data_list.append(data["lowest_price"])
-print(data_list)
+dict_list = []
+steamid = 76561198050601247
+filter = str(input("Input a filter: "))
+
+def getInventoryPrices(steamid, filter):
+    data = requests.get(
+        f"https://steamcommunity.com/profiles/{steamid}/inventory/json/730/2?l=english&count=5000")
+    json_data = data.json()
+    descriptions = json_data.get("rgDescriptions")
+    for item in descriptions:
+        if int(descriptions[item]["instanceid"]) != 0:
+            items = str(descriptions[item]["market_hash_name"])
+            item_list.append(items)
+        else:
+            continue
+    print(item_list)
+    print(len(item_list))
+    item_names = [item for item in item_list if filter in item]
+    time.sleep(69)
+    for item_name in item_names:
+        response = requests.get("https://steamcommunity.com/market/priceoverview/?"
+                                "appid=730&currency=3&market_hash_name="+item_name)
+        if response.status_code == 200:
+            data_market = response.json()
+            data_list.append(data_market["lowest_price"])
+            item_dict = {
+                item_name: data_market["lowest_price"]
+            }
+            dict_list.append(item_dict)
+        else:
+            continue
+
+    print(data_list)
+    print(dict_list)
+
+
+getInventoryPrices(steamid, filter)
+
