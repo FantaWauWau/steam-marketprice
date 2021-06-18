ee0538ebd0225002ed6d0e7b8f05db3b4667b21d
FantaWauWau <manuel.reynders@arcor.de>
2021-06-18 20:27:28 +0200

    Update joooo.py

--- joooo.py
+++ joooo.py
@@ -13,25 +13,4 @@ print(items)
 for number in range(0, len(items)):
     something = items[number]
     item_list.append(something)
-print(item_list[:])
-
-
-"""steamid = 76561198050601247
-
-item_list = []
-data = requests.get(
-    f"https://steamcommunity.com/profiles/{steamid}/inventory/json/730/2?l=english&count=5000")
-json_data = data.json()
-print(data.json())
-descriptions = json_data["rgDescriptions"]
-print(descriptions)
-print(type(descriptions))
-print(type(data.json()))
-
-for item in descriptions:
-    items = str(descriptions[item]['market_name'])
-    print(type(items))
-    item_list.append(items)
-print(item_list)
-print(type(item_list))
-"""
+print(item_list[4:])
