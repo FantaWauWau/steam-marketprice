c8a82c190c2106c4a923829ccba901d451ce7ec7
random241 <81753196+random241@users.noreply.github.com>
2021-07-04 01:21:25 +0200

    Update calculate_wear.py

--- calculate_wear.py
+++ calculate_wear.py
@@ -3,68 +3,73 @@ import csv
 from drop_check import drop_check
 
 def calculate_wear(case_name: str, quality: str, amount: int) -> None:
-    """Takes skins quality (color) and drop amount as arguments and calculates/writes drops in csv file"""
+    """Takes skins quality (color) and drop amount as arguments and calculates/writes skin_names_by_quality in csv file"""
+    # creates a list of dictionaries with content of csv file (opened case name)
     item_list = []
     with open(case_name, 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             item_list.append(row)
 
-    items_by_quality = [item for item in item_list if item['type'] == quality]
-    listed = []
+    # removes every dictionary from item_list which doesn't match the passed quality
+    items_by_quality = [item for item in item_list if item['quality'] == quality]
+
+    # creates a list with all skin names of quality x
+    all_skin_names_by_quality = []
     for item in range(0, len(items_by_quality)):
-        items_names = items_by_quality[item]['case']
-        listed.append(items_names)
+        skin_name = items_by_quality[item]['skin_name']
+        all_skin_names_by_quality.append(skin_name)
 
-    drops = []
+    # picks random skin names for total amount of drops of quality x
+    skin_names_by_quality = []
     for i in range(amount):
-        dropped = random.choice(listed)
-        drops.append(dropped)
-    global wear
-    wear = {}
-    for i in drops:
-        wear[i + " (Battle-Scarred)"] = 0
-        wear[i + " (Well-Worn)"] = 0
-        wear[i + " (Field-Tested)"] = 0
-        wear[i + " (Minimal Wear)"] = 0
-        wear[i + " (Factory New)"] = 0
+        random_skin_name = random.choice(all_skin_names_by_quality)
+        skin_names_by_quality.append(random_skin_name)
 
-    for item in drops:
+    # initializes dictionary with skin name and wear as key and drop count as value
+    global skin_name_with_wear
+    skin_name_with_wear = {}
+    for skin_name in skin_names_by_quality:
+        skin_name_with_wear[skin_name + " (Battle-Scarred)"] = 0
+        skin_name_with_wear[skin_name + " (Well-Worn)"] = 0
+        skin_name_with_wear[skin_name + " (Field-Tested)"] = 0
+        skin_name_with_wear[skin_name + " (Minimal Wear)"] = 0
+        skin_name_with_wear[skin_name + " (Factory New)"] = 0
+
+    # loops through skin names, calculates wear and checks if skin name with wear exists in current csv file (case)
+    # if skin exists drop count is raised by 1
+    for item in skin_names_by_quality:
         float = random.uniform(0, 1)
         while True:
             if 1 > float >= 0.45:
                 if drop_check(case_name, item + " (Battle-Scarred)"):
-                    wear[item + " (Battle-Scarred)"] += 1
+                    skin_name_with_wear[item + " (Battle-Scarred)"] += 1
                     break
-                else:
-                    continue
+                continue
             elif 0.45 > float >= 0.38:
                 if drop_check(case_name, item + " (Well-Worn)"):
-                    wear[item + " (Well-Worn)"] += 1
+                    skin_name_with_wear[item + " (Well-Worn)"] += 1
                     break
-                else:
-                    continue
+                continue
             elif 0.38 > float >= 0.15:
                 if drop_check(case_name, item + " (Field-Tested)"):
-                    wear[item + " (Field-Tested)"] += 1
+                    skin_name_with_wear[item + " (Field-Tested)"] += 1
                     break
-                else:
-                    continue
+                continue
             elif 0.15 > float >= 0.07:
                 if drop_check(case_name, item + " (Minimal Wear)"):
-                    wear[item + " (Minimal Wear)"] += 1
+                    skin_name_with_wear[item + " (Minimal Wear)"] += 1
                     break
-                else:
-                    continue
+                continue
             else:
                 if drop_check(case_name, item + " (Factory New)"):
-                    wear[item + " (Factory New)"] += 1
+                    skin_name_with_wear[item + " (Factory New)"] += 1
                     break
-                else:
-                    continue
+                continue
 
+    # writes result into cache.csv 
     with open('cache.csv', 'a', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['key', 'value']
+        fieldnames = ['skin_name', 'amount_of_skin_names_by_quality']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        for key, value in wear.items():
-            writer.writerow({'key': key, 'value': value})
+        for skin_name, amount in skin_name_with_wear.items():
+            writer.writerow({'skin_name': skin_name, 'amount_of_skin_names_by_quality': amount})
