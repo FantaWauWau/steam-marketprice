137c3173c1a4079c6ec75d07047983a837039c9d
random241 <81753196+random241@users.noreply.github.com>
2021-07-03 21:49:55 +0200

    Update case_simulator.py

--- case_simulator.py
+++ case_simulator.py
@@ -2,62 +2,29 @@ import csv
 import random
 import requests
 import time
-
+from calculate_wear import calculate_wear
 # CSGO Case Simulator
 
-def calculate_wear(quality: str, amount: int):
-    """Takes skins quality (color) and drop amount as arguments and calculates/writes drops in csv file"""
-    item_list = []
-    with open(case_name, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            item_list.append(row)
-
-    new_list = [item for item in item_list if item['type'] == quality]
-    listed = []
-    for item in range(0, len(new_list)):
-        items_names = new_list[item]['case']
-        listed.append(items_names)
-
-    drops = []
-    for i in range(amount):
-        dropped = random.choice(listed)
-        drops.append(dropped)
-    global wear
-    wear = {}
-    for i in drops:
-        wear[i + " (Well-Worn)"] = 0
-        wear[i + " (Battle-Scarred)"] = 0
-        wear[i + " (Field-Tested)"] = 0
-        wear[i + " (Minimal Wear)"] = 0
-        wear[i + " (Factory New)"] = 0
-
-    for i in drops:
-        rarity = random.uniform(0, 1)
-        if 1 > rarity >= 0.45:
-            wear[i + " (Battle-Scarred)"] += 1
-        elif 0.45 > rarity >= 0.38:
-                wear[i + " (Well-Worn)"] += 1
-        elif 0.38 > rarity >= 0.15:
-            wear[i + " (Field-Tested)"] += 1
-        elif 0.15 > rarity >= 0.07:
-            wear[i + " (Minimal Wear)"] += 1
-        else:
-            wear[i + " (Factory New)"] += 1
-
-    with open('cache.csv', 'a', newline='') as csvfile:
-        fieldnames = ['key', 'value']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        for key, value in wear.items():
-            writer.writerow({'key': key, 'value': value})
-
+def is_stattrack() -> bool:
+    """Returns True if random number <= 0.1, else False"""
+    stattrack_chance = random.uniform(0, 1)
+    if stattrack_chance <= 0.1:
+        return True
+    else:
+        return False
 
 dropped = {
     "blue": 0,
     "purple": 0,
     "pink": 0,
     "red": 0,
-    "yellow": 0
+    "yellow": 0,
+
+    "stat_blue": 0,
+    "stat_purple": 0,
+    "stat_pink": 0,
+    "stat_red": 0,
+    "stat_yellow": 0
     }
 
 # create csv file to store items names and drop amount later
@@ -103,15 +70,30 @@ opened = 0
 while opened < to_open:
     rarity = random.uniform(0, 1)
     if 1 > rarity >= 0.2007673:
-        dropped["blue"] += 1
+        if is_stattrack():
+            dropped["stat_blue"] += 1
+        else:
+            dropped["blue"] += 1
     elif 0.2007673 > rarity >= 0.0409208:
-        dropped["purple"] += 1
+        if is_stattrack():
+            dropped["stat_purple"] += 1
+        else:
+            dropped["purple"] += 1
     elif 0.0409208 > rarity >= 0.0089515:
-        dropped["pink"] += 1
+        if is_stattrack():
+            dropped["stat_pink"]
+        else:
+            dropped["pink"] += 1
     elif 0.0089515 > rarity >= 0.0025576:
-        dropped["red"] += 1
+        if is_stattrack():
+            dropped["stat_red"] += 1
+        else:
+            dropped["red"] += 1
     else:
-        dropped["yellow"] += 1
+        if is_stattrack():
+            dropped["stat_yellow"] += 1
+        else:
+            dropped["yellow"] += 1
     opened += 1
 
 # values of item quality drops into variables
@@ -121,11 +103,19 @@ pink = dropped["pink"]
 red = dropped["red"]
 yellow = dropped["yellow"]
 
-drop_dict = {"blue": blue, "purple": purple, "pink": pink, "red": red, "yellow": yellow}
+stat_blue = dropped["stat_blue"]
+stat_purple = dropped["stat_purple"]
+stat_pink = dropped["stat_pink"]
+stat_red = dropped["stat_red"]
+stat_yellow = dropped["stat_yellow"]
+
+drop_dict = {"blue": blue, "purple": purple, "pink": pink, "red": red, "yellow": yellow,
+            "stat_blue": stat_blue, "stat_purple": stat_purple, "stat_pink": stat_pink,
+            "stat_red": stat_red, "stat_yellow": stat_yellow}
 
 for key, value in drop_dict.items():
     if value != 0:
-        calculate_wear(key, value)
+        calculate_wear(case_name, key, value)
 
 # add items into new list, for multiple drops
 dropped_items = []
