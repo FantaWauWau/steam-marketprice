e76258bfe2fac10c80e89c6a2a93e30740022141
pingu <pingu241@protonmail.com>
2021-07-15 23:38:25 +0200

    moved append_failed_items() in functions

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import csv
 import random
 import os
+from http_codes import http_status_codes
 
 # dictionary for formatting case name for price request to steam
 market_case_name = {
@@ -241,3 +242,25 @@ def file_check():
         fieldnames = ["skin_name", "amount_of_drops"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
+
+
+def append_failed_items(skin_name, response_code, request_count):
+    to_write = {}
+    with open('failed_items.csv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if skin_name in row['skin_name'] and str(response_code) in row['response_code']:
+                print("NONE")
+                return None
+        to_write["skin_name"] = skin_name
+        to_write["response_code"] = response_code
+        to_write["request_count"] = request_count
+
+    with open('failed_items.csv', 'a', newline='', encoding='utf-8') as file:
+        fieldnames = ['skin_name', 'request_count', 'response_code', 'http_status_code']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow({'skin_name': to_write['skin_name'],
+                         'request_count': to_write['request_count'],
+                         'response_code': to_write['response_code'],
+                         'http_status_code': http_status_codes[response_code]
+                    })
